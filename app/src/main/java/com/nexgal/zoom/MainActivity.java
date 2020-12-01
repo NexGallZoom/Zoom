bfa6e65d1d21246f9e7002842c951eb77d4b0c72
SoonHyung_Lee <tnsgud9@naver.com>
2020-12-01 20:57:02 +0900

    Add CameraPreview

--- app/src/main/java/com/nexgal/zoom/MainActivity.java
+++ app/src/main/java/com/nexgal/zoom/MainActivity.java
@@ -11,17 +11,21 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.FrameLayout;
 
 import com.nexgal.zoom.features.camera.CameraManager;
+import com.nexgal.zoom.features.camera.CameraPreview;
 
 public class MainActivity extends AppCompatActivity {
     private final static int PERMISSION_REQUEST_CAMERA = 100001;
+    private CameraPreview cameraPreview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
                 return;
             }
@@ -38,13 +42,19 @@ public class MainActivity extends AppCompatActivity {
                     })
                     .show();
         }
+
+        Camera camera = manager.getCamera();
+        cameraPreview = new CameraPreview(this, camera);
+        FrameLayout preview = findViewById(R.id.camera_preview);
+        preview.addView(cameraPreview);
+
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case PERMISSION_REQUEST_CAMERA:
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // 권한이 승인된 경우 그리기
                     recreate();
                 } else {
